895dc5b609be259daacbea33adb2f5f6d4ba596b
udayhasan <udayhasan111@gmail.com>
2018-10-30 17:22:01 +0600

    forget id page edited

--- test_gui_self.py
+++ test_gui_self.py
@@ -1297,36 +1297,54 @@ class forgot_id_page(Frame):
 		super(forgot_id_page,self).__init__(master)
 		self.login_conn = sqlite3.connect('users.db')
 		self.login_cur  = self.login_conn.cursor()
-		self.grid()
+		self.pack()
 		self.define_widgets()
 
 	def define_widgets(self):
-		forgot_user_id=Label(self,text="Forgot User ID?")
-		forgot_user_id.grid(row=0,column=1,columnspan=2,sticky=W)
+		frame1 = Frame(self)
+		frame1.pack()
+		forgot_user_password=Label(frame1,text="::Forgot User Name?::")
+		forgot_user_password.config(width=200, font=("Courier", 25))
+		forgot_user_password.pack(pady=5)
 
-		forgot_id_email=Label(self,text="Email:")
-		forgot_id_email.grid(row=1,column=0,sticky=E)
+		canvas = Canvas(frame1, height=2, borderwidth=0, highlightthickness=0, bg="black")
+		canvas.pack(fill=X, padx=80, pady=10)
+
+		frame2 = Frame(self)
+		frame2.pack()
+
+		forgot_id_email=Label(frame2,text="Email:", width=10, anchor=W)
+		forgot_id_email.pack(side=LEFT, padx=2, pady=2)
 
 		self.forgot_id_email=StringVar()
-		forgot_id_email_entry=Entry(self,textvariable=self.forgot_id_email)
-		forgot_id_email_entry.grid(row=1,column=1,columnspan=3,sticky=W)
+		forgot_id_email_entry=Entry(frame2,textvariable=self.forgot_id_email, width=40)
+		forgot_id_email_entry.pack(side=LEFT, padx=2, pady=2)
 		forgot_id_email_entry.focus_set()
 
-		forgot_id_btn=Button(self,text="Find User Name", bg="DeepSkyBlue4", fg = "white", command=self.pre_forgot_id_find)
-		forgot_id_btn.grid(row=2,column=1,sticky=W)
+		frame3 = Frame(self)
+		frame3.pack()
 
-		back=Button(self,text="< Prev", command=self.go_prev)
-		back.grid(row=2,column=0,sticky=W)
+		forgot_id_btn=Button(frame3,text="Find", bg="DeepSkyBlue4", fg = "white", command=self.pre_forgot_id_find, width=10)
+		forgot_id_btn.pack(side=LEFT, padx=2, pady=2)
 
-		exit=Button(self,text="Exit", bg = "brown3", fg = "white", command=self.leave)
-		exit.grid(row=2,column=2,sticky=W)
+		back=Button(frame3,text="< Prev", command=self.go_prev, width=10)
+		back.pack(side=LEFT, padx=2, pady=2)
+
+		exit=Button(frame3,text="Exit", bg = "brown3", fg = "white", command=self.leave, width=10)
+		exit.pack(side=LEFT, padx=2, pady=2)
+
+		self.frame4 = Frame(self)
+		self.frame4.pack()
 
 	def create_user_table(self):
 		self.login_cur.execute("CREATE TABLE IF NOT EXISTS users(name TEXT, email TEXT, password TEXT)")
 
 	def pre_forgot_id_find(self):
-		self.forgot_id_name_label=Label(self, relief=RIDGE, fg="RED", text="<User ID>", state=DISABLED)
-		self.forgot_id_name_label.grid(row=3,column=1,sticky=W)
+		self.frame4.pack_forget()
+		self.frame4 = Frame(self)
+		self.frame4.pack()
+		self.forgot_id_name_label=Label(self.frame4, relief=RIDGE, text="<User ID>", state=DISABLED)
+		self.forgot_id_name_label.pack(side=LEFT, padx=2, pady=2)
 		self.forgot_id_find()
 
 	def forgot_id_find(self):
@@ -1339,7 +1357,7 @@ class forgot_id_page(Frame):
 			try:
 				self.login_cur.execute("SELECT name FROM users WHERE email = ?", (self.forgot_id_email.get(),))
 				id_name = self.login_cur.fetchall()
-				self.forgot_id_name_label.configure(text="Your ID is: "+str(id_name[0][0]), state=ACTIVE)
+				self.forgot_id_name_label.configure(text=":: "+str(id_name[0][0])+" ::", state=ACTIVE, fg="blue", font=("Courier", 25))
 			except Exception as e:
 				self.error_msg = "No such email address!"
 				print(e)
