730aa69efecce6f9039cf75a6c93b9c7418e44fa
udayhasan <udayhasan111@gmail.com>
2018-11-12 14:38:26 +0600

    remove admin added

--- test_gui_self.py
+++ test_gui_self.py
@@ -573,6 +573,7 @@ class edit_user_admin_sts_page(Frame):
 		canvas = Canvas(frame1, height=2, borderwidth=0, highlightthickness=0, bg="black")
 		canvas.pack(fill=X, padx=80, pady=10)
 		
+		#Add admin
 		frame2 = Frame(self)
 		frame2.pack()
 		
@@ -588,18 +589,35 @@ class edit_user_admin_sts_page(Frame):
 		assigned_to_list=OptionMenu(frame2,self.assigned_to_name, *name_list)
 		assigned_to_list.config(width=35)
 		assigned_to_list.pack(side=LEFT, padx=2, pady=5)
-		assigned_to_list.focus_set()
 
-		frame4 = Frame(self)
-		frame4.pack()
-
-		admin_btn = Button(frame4,text="Make Admin", command=self.make_admin_def, fg = 'white', bg = 'DeepSkyBlue4', width=10)
+		admin_btn = Button(frame2,text="Make Admin", command=self.make_admin_def, fg = 'white', bg = 'DeepSkyBlue4', width=10)
 		admin_btn.pack(side=LEFT, padx=2, pady=2)
 
-		back=Button(frame4,text="< Prev", command=self.go_prev, width=10)
+		#Remove from admin
+		if(self.name == "admin"):
+			frame3 = Frame(self)
+			frame3.pack()
+			
+			remove_from_label=Label(frame3,text="Remove Admin:", width=20, anchor=W)
+			remove_from_label.pack(side=LEFT, pady=5)
+
+			self.remove_from_name=StringVar()
+			self.remove_from_name.set('admin')
+
+			remove_from_list=OptionMenu(frame3,self.remove_from_name, *name_list)
+			remove_from_list.config(width=35)
+			remove_from_list.pack(side=LEFT, padx=2, pady=5)
+
+			remove_btn = Button(frame3,text="Remove Admin", command=self.remove_admin_def, fg = 'white', bg = 'DeepSkyBlue4', width=10)
+			remove_btn.pack(side=LEFT, padx=2, pady=2)
+
+		frameBelow = Frame(self)
+		frameBelow.pack()
+
+		back=Button(frameBelow,text="< Prev", command=self.go_prev, width=10)
 		back.pack(side=LEFT, padx=2, pady=2)
 
-		exit=Button(frame4,text="Exit", bg = "brown3", fg = "white", command=self.leave, width=10)
+		exit=Button(frameBelow,text="Exit", bg = "brown3", fg = "white", command=self.leave, width=10)
 		exit.pack(side=LEFT, padx=2, pady=2)
 
 	def create_user_table(self):
@@ -612,6 +630,23 @@ class edit_user_admin_sts_page(Frame):
 		else:
 			try:
 				self.login_cur.execute("UPDATE users SET admin_status = ? WHERE name = ?", ('1', self.assigned_to_name.get()[2:len(self.assigned_to_name.get())-3]))
+				self.login_conn.commit()
+				time.sleep(0.02)
+				print("User %s is made an Admin" %(self.assigned_to_name.get()))
+				self.screen = 3
+				self.quit()
+			except Exception:
+				self.error_msg = "Invalid search name or replacing name!"
+
+	def remove_admin_def(self):
+		self.create_user_table()
+		if(self.remove_from_name.get()==''):
+			self.error_msg = "Invalid input!"
+		elif(self.remove_from_name.get()=='admin'):
+			self.error_msg = "You cannot delete 'admin'"
+		else:
+			try:
+				self.login_cur.execute("UPDATE users SET admin_status = ? WHERE name = ?", ('0', self.assigned_to_name.get()[2:len(self.assigned_to_name.get())-3]))
 				self.login_conn.commit()
 				time.sleep(0.02)
 				print("User %s is made an Admin" %(self.assigned_to_name.get()))
